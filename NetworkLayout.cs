067030cb0356516400e54c2e2185882c1b0da412
Letosim <95633405+Letosim@users.noreply.github.com>
2024-11-28 17:07:19 +0100

    Add files via upload

---
+++ NetworkLayout.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkLayout
+{
+
+    public Neurode.NeurodeType type;
+    public bool usesMemory;
+
+
+
+    public int[] layout;
+    public int count;
+
+    public NetworkLayout(Neurode.NeurodeType type, int[] layout)
+    {
+        this.type = type;
+        //uses memory
+        //chromosomeCount
+        this.layout = layout;
+    }
+
+    public NetworkLayout(Neurode.NeurodeType type, int count)
+    {
+        this.type = type;
+        this.count = count;
+        layout = new int[] {};
+
+    }
+
+    //networkLayout[0] = new NetworkLayout(Neurode.NeurodeType.TanNeurode, new int[] { 0, 0, 0, 0, 0 });//crhomosen?
+    //networkLayout[1] = new NetworkLayout(Neurode.NeurodeType.TanNeurode, new int[] { 9, 0, 0, 0, 5 });//crhomosen?
+    //networkLayout[2] = new NetworkLayout(Neurode.NeurodeType.TanNeurode, new int[] { 0, 6, 6, 6, 0 });//crhomosen?
+
+
+    //networkLayout[0] = new NetworkLayout(Neurode.NeurodeType.TanNeurode, new int[] { 0{}, 0{}, 0{}, 0{}, 0{}});//crhomosen?
+    //networkLayout[1] = new NetworkLayout(Neurode.NeurodeType.TanNeurode, new int[] { 0{}, 0{}, 0{}, 0{}, 0{}});//crhomosen?
+    //networkLayout[2] = new NetworkLayout(Neurode.NeurodeType.TanNeurode, new int[] { 0{}, 0{}, 0{}, 0{}, 0{}});//crhomosen?
+
+
+
+}
