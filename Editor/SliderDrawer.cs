ef2834fd2ae496223bdd83d39c26ab6aa5fc1a40
Letosim <95633405+Letosim@users.noreply.github.com>
2024-12-02 23:13:00 +0100

    Delete Editor directory

--- Editor/SliderDrawer.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class SliderDrawer : MonoBehaviour
-{
-    // Show this float in the Inspector as a slider between 0 and 10
-    [Range(0.0F, 10.0F)]
-    public float myFloat = 0.0F;
-}
--- Editor/WindowReference.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEditor;
-
-public class WindowReference : ScriptableObject
-{
-    public EditorWindow linkedWindow;
-
-
-}
